7a070b9c324a760de042e9ec1c521e2773caf64a
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-06-29 10:13:43 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   104.js

--- 104.js
+++ 104.js
@@ -10,4 +10,10 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {};
+var maxDepth = function (root) {
+	let depth = 0;
+	if (root === null) {
+		return 0;
+	}
+	return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+};
