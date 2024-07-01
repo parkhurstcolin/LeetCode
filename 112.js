14d76247b3c74ca56e24a9964190ee209c3ef647
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-07-01 09:06:02 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   111.js
    	modified:   112.js

--- 112.js
+++ 112.js
@@ -8,19 +8,28 @@
  */
 /**
  * @param {TreeNode} root
- * @return {number}
+ * @param {number} targetSum
+ * @return {boolean}
  */
-var minDepth = function (root) {
-	// Base case...
-	// If the subtree is empty i.e. root is NULL, return depth as 0...
-	if (root == null) return 0;
-	// If the both subtrees are empty...
-	if (root.left == null && root.right == null) return 1;
-	// If the left subtree is empty, return the depth of right subtree after adding 1 to it...
-	if (root.left == null) return 1 + minDepth(root.right);
-	// If the right subtree is empty, return the depth of left subtree after adding 1 to it...
-	if (root.right == null) return 1 + minDepth(root.left);
-	// When the two child function return its depth...
-	// Pick the minimum out of these two subtrees and return this value after adding 1 to it...
-	return Math.min(minDepth(root.left), minDepth(root.right)) + 1; // Adding 1 is the current node which is the parent of the two subtrees...
+var hasPathSum = function (root, targetSum) {
+	let res = false;
+
+	function helper(node, currentSum) {
+		if (!node || res) {
+			return;
+		}
+
+		currentSum += node.val;
+
+		if (!node.left && !node.right && currentSum === targetSum) {
+			res = true;
+		}
+
+		helper(node.left, currentSum);
+		helper(node.right, currentSum);
+	}
+
+	helper(root, 0);
+
+	return res;
 };
