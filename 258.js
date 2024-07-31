925e785be34ee830aace2d94029f051e1a138b12
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-07-30 22:56:20 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   258.js

---
+++ 258.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+	if (num < 9) return num;
+	var n = 0;
+	let val = num.toString().split("");
+	while (val.length > 1) {
+		n = 0;
+		for (let y = 0; y < val.length; y++) {
+			n += +val[y];
+		}
+		val = n.toString().split("");
+	}
+	return n;
+};
