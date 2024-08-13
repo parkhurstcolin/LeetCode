89ad14ea9b8cfa54d7a27d56e0f5ff1885e6f216
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-08-13 10:23:51 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   500-599/504.js

---
+++ 500-599/504.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+	if (num === 0) return "0";
+
+	let temp = Math.abs(num);
+	let res = "";
+
+	while (temp !== 0) {
+		res = (temp % 7) + res;
+		temp = Math.floor(temp / 7);
+	}
+
+	return num >= 0 ? res : `-${res}`;
+};
