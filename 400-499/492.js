261891de8c40a5cfdef5113602e8eb9dabafe667
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-08-13 10:24:26 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   400-499/492.js

---
+++ 400-499/492.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+	let l = Math.floor(Math.sqrt(area));
+	let w = 0;
+	while (l * w != area) {
+		w = area / l;
+		if (!Number.isInteger(w)) l++;
+	}
+	if (l == 1) return [w, l];
+	return [l, w];
+};
