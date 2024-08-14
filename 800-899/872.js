764a28149c67c199fa6eefe005adebd7e1ee612e
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-08-14 09:25:36 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   800-899/872.js

---
+++ 800-899/872.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function (root1, root2) {
+	const collectLeafValues = (root, leafValues) => {
+		if (!root) {
+			return;
+		}
+		if (!root.left && !root.right) {
+			leafValues.push(root.val);
+		}
+		collectLeafValues(root.left, leafValues);
+		collectLeafValues(root.right, leafValues);
+	};
+
+	const leafValues1 = [];
+	const leafValues2 = [];
+	collectLeafValues(root1, leafValues1);
+	collectLeafValues(root2, leafValues2);
+
+	return JSON.stringify(leafValues1) === JSON.stringify(leafValues2);
+};
