43926d99bf6946aaef04919f85e9e78f6a5106cc
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-08-20 22:54:39 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   1-100/94.js

---
+++ 1-100/94.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+	if (root == null) return [];
+	let inOrder = [];
+
+	const traverse = (node) => {
+		if (node == null) return;
+		traverse(node.left);
+		inOrder.push(node.val);
+		traverse(node.right);
+	};
+	traverse(root);
+	return inOrder;
+};
