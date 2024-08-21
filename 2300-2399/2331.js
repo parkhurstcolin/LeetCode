c3a74ea409c54f92aecd844fbaec3dfc98a86283
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-08-20 22:26:02 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   2300-2399/2331.js

--- 2300-2399/2331.js
+++ 2300-2399/2331.js
@@ -16,17 +16,17 @@ var evaluateTree = function (root) {
 	if (root.val == 0) return false;
 	if (root.val == 1) return true;
 	if (root.val == 2) {
-		if (evaluateTree(root.left) || evaluateTree(root.right) == true) {
-			return true;
-		} else {
-			return false;
-		}
+			if (evaluateTree(root.left) || evaluateTree(root.right) == true) {
+				return true;
+			} else {
+				return false;
+			}
 	}
 	if (root.val == 3) {
-		if (evaluateTree(root.left) & (evaluateTree(root.right) == true)) {
-			return true;
-		} else {
-			return false;
-		}
+			if (evaluateTree(root.left) & evaluateTree(root.right)) {
+				return true;
+			} else {
+				return false;
+			}
 	}
 };
