fd102dfc38c8888a643794b77825a1592b534291
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-03 10:31:58 -0400

    434

---
+++ 400-499/434.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function (s) {
+	// Split the input string by spaces
+	const segments = s.split(" ");
+
+	let count = 0;
+	// Iterate through the segments and count non-empty ones
+	for (const segment of segments) {
+		if (segment !== "") {
+			count++;
+		}
+	}
+
+	return count;
+};
