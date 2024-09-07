c1ce3cdcb679f4f8ead7b1be8cdbe2f63c918bb1
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-07 11:02:30 -0400

    520

---
+++ 500-599/520.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function (word) {
+	return (
+		word === word.toUpperCase() ||
+		word === word[0] + word.substr(1).toLowerCase()
+	);
+};
