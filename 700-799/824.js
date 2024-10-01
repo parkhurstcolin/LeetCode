f3224ed9f6eb99e1cb17b3d00ae1f344339f4333
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-30 22:32:08 -0400

    824

---
+++ 700-799/824.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function (sentence) {
+	const vowelSet = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+	return sentence
+		.split(" ")
+		.map((str, i) => {
+			return (
+				(vowelSet.has(str[0]) ? str + "ma" : str.substring(1) + str[0] + "ma") +
+				"a".repeat(i + 1)
+			);
+		})
+		.join(" ");
+};
