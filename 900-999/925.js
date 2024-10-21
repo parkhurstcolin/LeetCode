16ed1ec4d4796214637d10ccdb5a38a5060b3ad4
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-20 21:40:39 -0400

    925

---
+++ 900-999/925.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function (name, typed) {
+	let namePointer = 0;
+	let typedPointer = 0;
+
+	if (typed.length < name.length) return false;
+
+	while (typedPointer < typed.length) {
+		if (name[namePointer] === typed[typedPointer]) {
+			namePointer += 1;
+			typedPointer += 1;
+		} else if (name[namePointer - 1] === typed[typedPointer]) {
+			typedPointer += 1;
+		} else {
+			return false;
+		}
+	}
+
+	return name.length === namePointer;
+};
