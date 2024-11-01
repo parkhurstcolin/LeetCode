0410a0ba2b61b4869c3ac1a7aedd4312122716c1
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-11-01 11:16:32 -0400

    1360

---
+++ 1300-1399/1360.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} date1
+ * @param {string} date2
+ * @return {number}
+ */
+const daysBetweenDates = (date1, date2) => {
+	const millisecondsInADay = 1000 * 60 * 60 * 24;
+	return Math.abs(
+		(new Date(date1).getTime() - new Date(date2).getTime()) / millisecondsInADay
+	);
+};
