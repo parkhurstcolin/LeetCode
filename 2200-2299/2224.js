cd8e687deeccb128256f5e4cb219b7b31c5c0562
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-12 11:16:35 -0500

    Final refactor

--- 2200-2299/2224.js
+++ 2200-2299/2224.js
@@ -3,27 +3,23 @@
  * @param {string} correct
  * @return {number}
  */
-
 var convertTime = function (current, correct) {
-  let operations = 0;
   current = current.split(":");
   correct = correct.split(":");
-  let HH = Number(correct[0] * 60) - Number(current[0] * 60);
-  let MM = Number(correct[1]) - Number(current[1]);
+  let HH = Number(correct[0] - current[0]) * 60;
+  let MM = Number(correct[1] - current[1]);
   let num = HH + MM;
+  let operations = 0;
   while (num != 0) {
+    operations++;
     if (num >= 60) {
       num -= 60;
-      operations++;
     } else if (num >= 15) {
       num -= 15;
-      operations++;
     } else if (num >= 5) {
       num -= 5;
-      operations++;
     } else if (num >= 1) {
       num -= 1;
-      operations++;
     }
   }
   return operations;
