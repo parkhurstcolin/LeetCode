1a969b9de66c6f467452796880b16aece91174ca
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-12 11:11:08 -0500

    2138 wrong

---
+++ 2100-2199/2138.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @param {character} fill
+ * @return {string[]}
+ */
+var divideString = function (s, k, fill) {
+  s = s.split("");
+  let store = [];
+  while (s.length != 0) {
+    let word = "";
+    let x = k;
+    while (x != 0) {
+      if (s.length != 0) word += s.shift();
+      else word += "x";
+      x--;
+    }
+    store.push(word);
+  }
+  return store;
+};
