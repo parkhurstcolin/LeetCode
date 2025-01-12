a720f9b94300b8f3e8a1ba1ef8948e30b9ef6e4d
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-12 11:13:51 -0500

    2259

--- 2200-2299/2259.js
+++ 2200-2299/2259.js
@@ -4,16 +4,13 @@
  * @return {string}
  */
 var removeDigit = function (number, digit) {
-  let num = number.split("");
-  let n = "";
-  let store = "";
-  for (let y = 0; y < num.length + 1; y++) {
-    if (num[y] == digit) {
-      num[y] = "";
-      store = num.join("");
-      if (Number(n) < Number(store)) n = store;
-      num = number.split("");
+    let last;
+
+    for (let i = 0; i < number.length; i++) {
+      if (number[i] !== digit) continue;
+      last = i;
+      if (number[i + 1] <= digit) continue;
+      return number.slice(0, i) + number.slice(i + 1);
     }
-  }
-  return n;
+    return number.slice(0, last) + number.slice(last + 1);
 };
