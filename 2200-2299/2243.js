853c09947d8e8fbe9a04cef8dcfea3609fb30fce
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-31 10:42:41 -0500

    2243

---
+++ 2200-2299/2243.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var digitSum = function (s, k) {
+  while (s.length > k) {
+    let newString = "";
+    for (let i = 0; i < s.length; i += k)
+      newString += s
+        .substring(i, i + k)
+        .split("")
+        .reduce((acc, val) => acc + +val, 0);
+
+    s = newString;
+  }
+
+  return s;
+};
